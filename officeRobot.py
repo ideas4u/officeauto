032305a9c2bebaa4c957ddf261927fc30e796d6c
ideas4u <xiewygz@m.scnu.edu.cn>
2015-11-18 15:15:43 +0800

    commit all change to the repo

--- officeRobot.py
+++ officeRobot.py
@@ -14,6 +14,7 @@ class Officerobot(object):
 		self.prefesion = prefesion
 		
 	def print_name(self):
+		"""print out the name and prefesion of the instance"""
 		print("name: %s "% (self.name))
 		print('Prefesion: %s ' % (self.prefesion))
 	
